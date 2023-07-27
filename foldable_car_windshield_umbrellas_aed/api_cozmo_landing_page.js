d03714c08332f6580b2005fcb7ae727f9b342e60
omar anajar <omaranajar4@gmail.com>
2023-07-28 00:19:11 +0800

    Changes in car umbrellas product

--- foldable_car_windshield_umbrellas_aed/api_cozmo_landing_page.js
+++ foldable_car_windshield_umbrellas_aed/api_cozmo_landing_page.js
@@ -11,8 +11,59 @@ $("#formInfo").submit(function (event) {
   var phone = $('#formInfo input[name="phone"]').val();
   var adresse = $('#formInfo input[name="adresse"]').val();
 
-  // Send an AJAX request to insert the order record
+  // Create the data object for SheetDB
+  var sheetDBData = {
+    name: "Foldable Car Windshield Umbrellas",
+    date: new Date().toString(),
+    customer_name: fullname,
+    phone: phone,
+    city: adresse,
+    address: adresse,
+    quantity: "1",
+    price: "396.48 MAD",
+    product_notice: "",
+    notice: "",
+    status: "pending",
+    fees_shipping: "",
+  };
 
+  // Insert into SheetDB API
+  fetch("https://sheetdb.io/api/v1/szh8pfhm2hmh5", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ data: sheetDBData }),
+  })
+    .then(function (response) {
+      console.log("sent");
+      if (response.ok) {
+        // Handle successful response from SheetDB
+        console.log("Order added to SheetDB successfully");
+
+        // To track the purchase event using Facebook Pixel
+        fbq("track", "Purchase", {
+          value: 39,
+          currency: "USD",
+          content_name:
+            "Foldable Car Windshield Umbrellas, Large Size Car Windshield Sun Shade Umbrella for Front Windows, Suitable for Windshields of Various Car Models",
+          content_type: "Car Accessories",
+          product_id: "1121",
+        });
+      } else {
+        // Handle error response from SheetDB
+        console.log("Failed to add order to SheetDB");
+        // throw new Error("Failed to add order to SheetDB");
+      }
+    })
+    .catch(function (error) {
+      console.log("NOT sent");
+      console.log("Error:", error);
+      // Display an error message if the request fails
+      // alert("Failed to add order to SheetDB. Please try again later.");
+    });
+
+  // Send an AJAX request to insert the order record
   $.ajax({
     url: "https://noxeva.com/api/ordervisite",
     type: "POST",
@@ -36,14 +87,14 @@ $("#formInfo").submit(function (event) {
     },
     success: function (response) {
       // To track the purchase event using Facebook Pixel
-      fbq("track", "Purchase", {
-        value: 39,
-        currency: "USD",
-        content_name:
-          "Foldable Car Windshield Umbrellas, Large Size Car Windshield Sun Shade Umbrella for Front Windows, Suitable for Windshields of Various Car Models",
-        content_type: "Car Accessories",
-        product_id: "1121",
-      });
+      // fbq("track", "Purchase", {
+      //   value: 39,
+      //   currency: "USD",
+      //   content_name:
+      //     "Foldable Car Windshield Umbrellas, Large Size Car Windshield Sun Shade Umbrella for Front Windows, Suitable for Windshields of Various Car Models",
+      //   content_type: "Car Accessories",
+      //   product_id: "1121",
+      // });
 
       document.location.href =
         "/foldable_car_windshield_umbrellas_aed/order_success.html";
@@ -51,17 +102,6 @@ $("#formInfo").submit(function (event) {
       // hide loading icon and enable the button
       // $("#save_guest_order").prop("disabled", false);
       // $("#span_loading").hide();
-      console.log("response", response);
-      // swal({
-      //   title: "تمت الطلبية بنجاح!",
-      //   text: "سيتصل بك قريقنا لتأكيد الطلبية",
-      //   icon: "success",
-      //   buttons: {
-      //     confirm: {
-      //       className: "btn btn-success",
-      //     },
-      //   },
-      // });
     },
     error: function (xhr, status, error) {
       // hide loading icon and enable the button
@@ -69,7 +109,9 @@ $("#formInfo").submit(function (event) {
       $("#span_loading").hide();
       console.log("Error :", error);
       // Display an error message if the update fails
-      alert("وقع حطأ اثناء الطلب , يرجى المحاولة لاحقا ");
+      // alert("وقع حطأ اثناء الطلب , يرجى المحاولة لاحقا ");
+      document.location.href =
+        "/foldable_car_windshield_umbrellas_aed/order_success.html";
     },
   });
 });
