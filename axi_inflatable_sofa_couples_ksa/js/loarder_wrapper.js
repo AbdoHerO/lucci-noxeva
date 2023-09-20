f316bcc550379d8c3a32cd0c8db5189e1416a73a
omar anajar <omaranajar4@gmail.com>
2023-09-20 22:59:53 +0400

    TikTOK ADS Pixel Setup

--- axi_inflatable_sofa_couples_ksa/js/loarder_wrapper.js
+++ axi_inflatable_sofa_couples_ksa/js/loarder_wrapper.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", function () {
   $(".loader").hide();
   $("body").removeClass("body_fixed");
+
+  // To track the ViewContent event using TikTok Pixel
+  ttq.track("ViewContent");
 });
 
 // document.addEventListener("DOMContentLoaded", function () {
